9e6081b1a48f773e73cc481f86bfb8089494de21
prabudhsarate12-cyber <prabudh.sarate12@gmail.com>
2026-03-28 16:38:20 +0530

    Initial commit

---
+++ src/test/java/com/dclaimpro/DclaimproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dclaimpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DclaimproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
